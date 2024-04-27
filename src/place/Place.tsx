ac7bc91e0bfa58337f448c3ca19ff730ffdbb893
SofyaNovikova <sofinovikova54@gmail.com>
2024-04-27 12:09:34 +0300

    map

--- src/place/Place.tsx
+++ src/place/Place.tsx
@@ -3,29 +3,54 @@ import * as TreeImage from "./tree.png";
 import * as InstaIcon from "./instagram.png";
 import * as MapIcon from "./map.png";
 import "./Place.css";
+import { Map, YMaps, Placemark } from "@pbe/react-yandex-maps";
+
+const coords = [54.048309, 27.083714];
 
 export const Place = () => {
-
   return (
-    <div className="place-container">
-      <p className="title">Место проведения</p>
-      <Banner background="#F9E6A5">
-        <div className="place-content">
-          <p className="text">
-            Праздник состоится на территории усадьбы "Ганка". Мы свяжемся с вами
-            заранее, чтобы договориться о трансфере к месту проведения.
-          </p>
-          <div className="links">
-            <a className="link" href="https://www.instagram.com/usadbaganka/" target="blank" rel="noreferrer">
-                <img className="icon instagram" src={InstaIcon.default} alt="insta" />
-            </a>
-            <a className="link" href="https://yandex.by/maps/-/CDVoj-9a" target="_blank" rel="noreferrer">
+    <>
+      <div className="place-container">
+        <p className="title">Место проведения</p>
+        <Banner background="#F9E6A5">
+          <div className="place-content">
+            <p className="text">
+              Праздник состоится на территории усадьбы "Ганка". Мы свяжемся с
+              вами заранее, чтобы договориться о трансфере к месту проведения.
+            </p>
+            <div className="links">
+              <a
+                className="link"
+                href="https://www.instagram.com/usadbaganka/"
+                target="blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="icon instagram"
+                  src={InstaIcon.default}
+                  alt="insta"
+                />
+              </a>
+              <a
+                className="link"
+                href="https://yandex.by/maps/-/CDVoj-9a"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <img className="icon map" src={MapIcon.default} alt="map" />
-            </a>
+              </a>
+            </div>
           </div>
-        </div>
-      </Banner>
-      <img src={TreeImage.default} alt="oak" className="tree-image" />
-    </div>
+        </Banner>
+        <img src={TreeImage.default} alt="oak" className="tree-image" />
+      </div>
+      <div className="map">
+        <YMaps>
+          <Map width="100%" defaultState={{ center: coords, zoom: 15 }}>
+            <Placemark geometry={coords} />
+          </Map>
+        </YMaps>
+      </div>
+    </>
   );
 };
